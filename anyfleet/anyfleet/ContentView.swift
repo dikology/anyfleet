39aa8cfc9f24730657421c31ba97e08737b046e1
dikology <dgimaletdinov@gmail.com>
2025-12-11 11:57:00 +0300

    init app project

---
+++ anyfleet/anyfleet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  anyfleet
+//
+//  Created by Денис on 12/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ anyfleet/anyfleet/anyfleetApp.swift
@@ -0,0 +1,17 @@
+//
+//  anyfleetApp.swift
+//  anyfleet
+//
+//  Created by Денис on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct anyfleetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
